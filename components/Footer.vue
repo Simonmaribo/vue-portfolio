fc7c5fbef4e82815c5111d544e0e20906dc2d408
Simon Maribo <simonmaribo@outlook.dk>
2022-09-15 17:42:12 +0200

    Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,28 @@
 <template>
     <div class="footer">
-        <p>Built with 3 by Simon Maribo</p>
+        <p>Made with <span class="heart">❤</span> by <a href="https://github.com/Simonmaribo/vue-portfolio" target="_blank">Simon Maribo</a></p>
     </div>
 </template>
 
 <style scoped>
     .footer {
+        padding: 1rem;
         display: flex;
-        flex-direction: row;
-        justify-content: space-between;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .footer p {
+        font-weight: 500;
+        color: var(--primary-color);
+    }
+    .footer p .heart {
+        color: #e25555;
+    }
+    .footer p a {
+        font-weight: 700;
+        color: var(--primary-color);
+        text-decoration: none;
     }
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,21 @@
     <div>
         <ProfileSection/>
         <ProjectSection/>
+        <Footer/>
     </div>
 </template>
   
 <script lang="ts">
 import ProfileSection from '~~/components/sections/ProfileSection/index.vue';
 import ProjectSection from '~~/components/sections/ProjectSection/index.vue';
+import Footer from '~~/components/Footer.vue';
+
 export default ({
   name: "IndexPage",
   components: {
     ProfileSection,
-    ProjectSection
+    ProjectSection,
+    Footer,
 } 
 })
 </script>
