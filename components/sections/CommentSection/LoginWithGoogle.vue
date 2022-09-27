d2029f9f6439dd5df7a1ea9b3b6cf0cfeab585c6
Simon Maribo <simonmaribo@outlook.dk>
2022-09-27 17:03:03 +0200

    conf or smth

--- components/sections/CommentSection/LoginWithGoogle.vue
+++ components/sections/CommentSection/LoginWithGoogle.vue
@@ -5,26 +5,29 @@
 </template>
 
 <script>
-    export default {
-        name: "GoogleButton",
-        methods: {
-            loginWithGoogle() {
-                var popup = window.open(
-                    'http://localhost:3000/api/auth/login',
-                    'popUpWindow',
-                    'height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes'
-                );
-                var timeout = setInterval(() => {
-                    try {
-                        if(popup.location.origin === "http://localhost:3000") {
-                            popup.close();
-                            this.$emit('authenticated');
-                        }
-                    } catch (error) {}
 
-                    if(popup.closed) clearInterval(timeout);
-                }, 1000);
-            }
+import conf from '../../../conf.json'
+
+export default {
+    name: "GoogleButton",
+    methods: {
+        loginWithGoogle() {
+            var popup = window.open(
+                `${conf.domain}/api/auth/login`,
+                'popUpWindow',
+                'height=500,width=400,left=100,top=100,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no, status=yes'
+            );
+            var timeout = setInterval(() => {
+                try {
+                    if(popup.location.origin === conf.domain) {
+                        popup.close();
+                        this.$emit('authenticated');
+                    }
+                } catch (error) {}
+
+                if(popup.closed) clearInterval(timeout);
+            }, 1000);
         }
     }
+}
 </script>
