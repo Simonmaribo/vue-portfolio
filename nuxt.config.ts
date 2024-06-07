06655f4b10f27f393e246edd467caa6d14b5bfed
Simon Maribo <simonmaribo@outlook.dk>
2024-06-07 23:40:34 +0200

    Added Toolbird

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,32 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        'nuxt-icon'
+  modules: ["nuxt-icon"],
+  meta: {
+    title: "Simon Maribo | Portfolio",
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/simonmaribo.png" },
+      {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css",
+      },
     ],
-    meta: {
-        title: "Simon Maribo | Portfolio",
-        link: [
-            { rel: "icon", type: "image/x-icon", href: "/simonmaribo.png" },
-            { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"}
-        ],
-        script: [
-            { src: "https://cdn.splitbee.io/sb.js", async: true },
-            { src: "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" },
-        ]
+    script: [
+      { src: "https://cdn.splitbee.io/sb.js", async: true },
+      {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js",
+      },
+      {
+        src: "https://api.toolbird.io/js/script.js",
+        async: true,
+        "data-domain": "simonmaribo.dk",
+      },
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      baseUrl: "",
     },
-    runtimeConfig: {
-        public: {
-            baseUrl: ''
-        }
-    }
-})
+  },
+});
